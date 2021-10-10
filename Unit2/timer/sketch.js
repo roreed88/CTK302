c8cad3e4fab3585594a2bdcb2c3c1f2db92014d2
roreed88 <89104537+roreed88@users.noreply.github.com>
2021-10-10 14:53:31 -0500

    10/10/21 2

--- Unit2/timer/sketch.js
+++ Unit2/timer/sketch.js
@@ -34,11 +34,11 @@ function draw() {
   }
     break;
   }
-  timer++;
-  //5*60 = 5 seconds because draw renders 60 times per second
-  //somethign about frame rate refreshing and 229 and 360?
-  if(timer >= 5*60){
-    timer = 0 ;
-    print("beep") ;
-  }
+  // timer++;
+  // //5*60 = 5 seconds because draw renders 60 times per second
+  // //somethign about frame rate refreshing and 229 and 360?
+  // if(timer >= 5*60){
+  //   timer = 0 ;
+  //   print("beep") ;
+  // }
 }
